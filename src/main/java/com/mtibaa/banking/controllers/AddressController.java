1842b6f660bb2134e8a4fae8984635cc701a201e
mtibamahdi <mtiba.mahdi@gmail.com>
2023-02-10 07:54:39 +0100

    adding files

--- src/main/java/com/mtibaa/banking/controllers/AddressController.java
+++ src/main/java/com/mtibaa/banking/controllers/AddressController.java
@@ -1,15 +1,46 @@
 package com.mtibaa.banking.controllers;
 
+import com.mtibaa.banking.dto.AddressDto;
 import com.mtibaa.banking.services.AddressService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/address")
+@RequestMapping("/addresses")
 @RequiredArgsConstructor
 public class AddressController {
 
-    private final AddressService addressService;
+    private final AddressService service;
+
+    @PostMapping("/")
+    public ResponseEntity<Integer> save(
+            @RequestBody AddressDto addressDto
+            ){
+        return ResponseEntity.ok(service.save(addressDto));
+    }
+    @GetMapping("/")
+    public ResponseEntity<List<AddressDto>> findAll(){
+        return ResponseEntity.ok(service.findAll());
+    }
+
+    @GetMapping("/{address-id}")
+    public ResponseEntity<AddressDto> findById(
+            @PathVariable("address-id") Integer addressId
+    ){
+        return ResponseEntity.ok(service.findById(addressId));
+    }
+
+    @DeleteMapping("/{address-id}")
+    public ResponseEntity<Void> delete(
+            @PathVariable("address-id") Integer addressId
+    ){
+        service.delete(addressId);
+        return ResponseEntity.accepted().build();
+    }
+
+
 
 }
