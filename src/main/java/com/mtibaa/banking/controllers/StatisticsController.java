13c0a11c6ac3f8ed3ed18fb2b6ac273737026309
mtibamahdi <mtiba.mahdi@gmail.com>
2023-02-11 06:51:08 +0100

    adding files

---
+++ src/main/java/com/mtibaa/banking/controllers/StatisticsController.java
@@ -0,0 +1,47 @@
+package com.mtibaa.banking.controllers;
+
+import com.mtibaa.banking.services.StatisticsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/statistics")
+public class StatisticsController {
+
+    private final StatisticsService service;
+    @GetMapping("/sum-by-date/{user-id}")
+    public ResponseEntity<Map<LocalDate,BigDecimal>> findSumTransactionsByDate(
+            @PathVariable("user-id") Integer userId,
+            @RequestParam("start-date") LocalDate startDate,
+            @RequestParam("end-date") LocalDate endDate
+    ){
+        return ResponseEntity.ok(service.findSumTransactionsByDate(startDate,endDate,userId));
+    }
+    @GetMapping("/account-balance/{user-id}")
+    ResponseEntity<BigDecimal> getAccountBalance(
+            @PathVariable("user-id") Integer userId
+    ){
+        return ResponseEntity.ok(service.getAccountBalance(userId));
+    }
+
+    @GetMapping("/highest-transfer/{user-id}")
+    ResponseEntity<BigDecimal> highestTransfer(
+            @PathVariable("user-id") Integer userId
+    ){
+        return ResponseEntity.ok(service.highestTransfer(userId));
+    }
+
+    @GetMapping("/highest-deposit/{user-id}")
+    ResponseEntity<BigDecimal> highestDeposit(
+            @PathVariable("user-id") Integer userId
+    ){
+        return ResponseEntity.ok(service.highestDeposit(userId));
+    }
+
+}
