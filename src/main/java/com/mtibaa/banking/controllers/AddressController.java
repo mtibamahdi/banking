9f87466df32abaeddd1eb04e73ae26ff72291abf
mtibamahdi <mtiba.mahdi@gmail.com>
2023-02-11 06:58:20 +0100

    adding files

--- src/main/java/com/mtibaa/banking/controllers/AddressController.java
+++ src/main/java/com/mtibaa/banking/controllers/AddressController.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/addresses")
 @RequiredArgsConstructor
+@RequestMapping("/addresses")
 public class AddressController {
 
     private final AddressService service;
